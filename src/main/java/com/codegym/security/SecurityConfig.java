e7da6dd7633dcbb56d9e78e8d616e9ae99f5cf6d
vulinh1704 <truongdangvulinh1704@gmail.com>
2023-12-04 08:27:43 +0700

    update securityFilterChain

--- src/main/java/com/codegym/security/SecurityConfig.java
+++ src/main/java/com/codegym/security/SecurityConfig.java
@@ -14,18 +14,19 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -77,25 +78,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.httpBasic().authenticationEntryPoint(restServicesEntryPoint())
-                .and().csrf().disable().authorizeRequests()
-                .requestMatchers("/login", "/register", "/hello").permitAll()
-                .requestMatchers("/users/**").access("hasRole('ROLE_USER')")
-                .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-//                .requestMatchers(HttpMethod.GET).access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-//                .requestMatchers(HttpMethod.DELETE, "/categories",
-//                        "/typeOfQuestions",
-//                        "/questions",
-//                        "/answers",
-//                        "/quizzes",
-//                        "/hello").access("hasRole('ROLE_ADMIN')")
-//                .requestMatchers(HttpMethod.PUT, "/users")
-//                .access("hasRole('ROLE_USER')")
-                .anyRequest().authenticated().and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .and().addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler())
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().cors()
-                .and().build();
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/register", "/hello").permitAll()
+                        .requestMatchers("/users/**").hasAnyAuthority("ROLE_USER")
+                        .requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
+                )
+                .exceptionHandling(customizer -> customizer.accessDeniedHandler(customAccessDeniedHandler()))
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
