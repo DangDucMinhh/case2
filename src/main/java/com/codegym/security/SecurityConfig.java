43fcc45ebee0a372df6b52314048e04c1d6cb1aa
vulinh1704 <truongdangvulinh1704@gmail.com>
2023-12-04 08:51:18 +0700

    update securityFilterChain

--- src/main/java/com/codegym/security/SecurityConfig.java
+++ src/main/java/com/codegym/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.codegym.security.jwt.JwtAuthenticationFilter;
 import com.codegym.security.jwt.RestAuthenticationEntryPoint;
 import com.codegym.service.UserService;
 import com.codegym.service.impl.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,9 +14,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,7 +34,6 @@ public class SecurityConfig {
     public UserService userService() {
         return new UserServiceImpl();
     }
-
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
@@ -78,9 +74,17 @@ public class SecurityConfig {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/register", "/hello").permitAll()
-                        .requestMatchers("/users/**").hasAnyAuthority("ROLE_USER")
-                        .requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
+                                .requestMatchers("/login", "/register", "/hello").permitAll()
+                                .requestMatchers("/users/**").hasAnyAuthority("ROLE_USER")
+                                .requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
+//                        .requestMatchers(HttpMethod.GET).hasAnyRole("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+//                        .requestMatchers(HttpMethod.DELETE, "/categories",
+//                                "/typeOfQuestions",
+//                                "/questions",
+//                                "/answers",
+//                                "/quizzes",
+//                                "/hello").hasAnyAuthority("ROLE_ADMIN")
+//                        .requestMatchers(HttpMethod.PUT, "/users").hasAnyAuthority("ROLE_USER")
                 )
                 .exceptionHandling(customizer -> customizer.accessDeniedHandler(customAccessDeniedHandler()))
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
